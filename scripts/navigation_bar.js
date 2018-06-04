34f70695d45a1ede33d7700d3a42999c3ba78a07
Amy Jiang <amyjianggg@gmail.com>
2018-06-03 18:25:52 -0700

    fix scroll/resize options depending on media width

--- scripts/navigation_bar.js
+++ scripts/navigation_bar.js
@@ -1,28 +1,55 @@
 /**ANIMATE HEADER**/
+
  $(document).ready(function() { 
+     var scroll_height = $(window).scrollTop();
+     var window_width = $(window).width();
+     
     $('.toggle').click(function(){ /*toggles when specificed width size*/
         $('ul').toggleClass('active');
     })
-    $(window).scroll( function() { /*change style of header when scrolling*/
-        var value = $(this).scrollTop();
-        if (value > 80){
+     
+    $(window).scroll( function() {
+        scroll_height = $(this).scrollTop();
+        animate_header();
+
+    })
+    $(window).resize( function() {
+        window_width = $(this).width();
+        animate_header();
+    });
+     
+    function animate_header() {
+        if (scroll_height > 80){ /*scroll down*/ 
+            if (window_width > 1250) {
+                $('ul#navigation_bar').css('margin-top', '');
+            }
+            else {
+                $('ul#navigation_bar').css('margin-top', '50px');
+                $('button.hamburger').css('margin-top', '-1em');
+
+            }
+            
             $("header").css('height', '50px');
             $('p.title').html('');
             $("img#logo").css('width' , '126px');
             $("p.title").css('font-size', '17px'); 
             $("ul li a").css('padding', '4.7px 20px');
-            $('ul#navigation_bar').css('margin-top', '50px');
-            $('button.hamburger').css('margin-top', '-1em');
         }
-        else {
+        else { /*scroll up*/ 
+            if (window_width > 1250 ) {
+                $('ul#navigation_bar').css('margin-top', '');
+            } 
+            else {
+                $('ul#navigation_bar').css('margin-top', '80px');
+                $('button.hamburger').css('margin', '0em -1em');
+            }
+            
             $("header").css('height', '80px');
             $("img#logo").css('width' , '200px');
             $("p.title").css('font-size', '22px'); 
             $('p.title').html('Women in Mathematics <br / >at Berkeley');
-            $("ul li a").css('padding', '10px 20px')
-            $('ul#navigation_bar').css('margin-top', '5em');
-            $('button.hamburger').css('margin', '0em -1em');
+            $("ul li a").css('padding', '10px 20px');
+        
         }
-
-    })
+    }
 });
